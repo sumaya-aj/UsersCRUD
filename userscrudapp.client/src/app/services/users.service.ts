4327d4cb0fcd7f5a285185b76a1921009ca2552d
Sumaya Ahmad <sumayajaabo07@gmail.com>
2024-06-26 15:25:59 +0300

    get url change

--- userscrudapp.client/src/app/services/users.service.ts
+++ userscrudapp.client/src/app/services/users.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environment';
 import { User } from '../types/user';
 
 @Injectable({
@@ -10,10 +11,11 @@ export class UsersService {
 
   users: User[] = [];
   private getAllUsersUrl = 'http://localhost:5030/api/Users/GetAll'; // add to some global config file
+  private baseAPIUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
   getAllUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.getAllUsersUrl);
+    return this.http.get<User[]>(`${this.baseAPIUrl}/GetAll`);
   }
 }
